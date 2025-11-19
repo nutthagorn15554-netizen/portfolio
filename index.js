cdea52eb7162cf7daa9507c560991c875ed67da9
nutthagorn15554-netizen <nutthagorn15554@gmail.com>
2025-11-20 01:47:05 +0700

    <MESSAGE>

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+});
+
+
 function UserInput() {
     userInput = document.getElementById("user-input").value
     console.log(userInput)
@@ -78,14 +91,3 @@ function PlayGame() {
 
 }
 
-const openBtn = document.getElementById("openModal");
-const closeBtn = document.getElementById("closeModal");
-const modal = document.getElementById("modal");
-
-openBtn.addEventListener("click", () => {
-    modal.classList.add("open");
-});
-
-closeBtn.addEventListener("click", () => {
-    modal.classList.remove("open");
-});
