a6a2fc757be752cd71907eef53cfc55148067dd1
nutthagorn15554-netizen <nutthagorn15554@gmail.com>
2025-11-20 01:01:13 +0700

    <MESSAGE>

--- index.js
+++ index.js
@@ -77,3 +77,15 @@ function PlayGame() {
     }
 
 }
+
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+});
